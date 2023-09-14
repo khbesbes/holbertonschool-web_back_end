170b259db57b69d87d9b2323010b879466860bd7
khbesbes <5786@holbertonstudents.com>
2023-09-14 15:04:28 +0100

    2-arrow.js

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,8 +1,9 @@
-export default function getNeighborhoodsList() {
+xport default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood =  add =>(newNeighborhood) 
+  this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
+  };
 }
