36f76159ad36c278b0df0c254e48c7a705ef6d12
khbesbes <5786@holbertonstudents.com>
2023-09-14 14:30:57 +0100

    1-block-scoped.js

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
     const task = false;
     const task2 = true;
-    
-      if (trueOrFalse) {
-        const newtask = false;
-        const newtask2 = true;
-        return [newtask, newtask2];
-      }
-    
-    return [task, task2];
+  
+    if (trueOrFalse) {
+      const newtask = true;
+      const newtask2 = false;
+      return [newtask, newtask2];
     }
+
+    return [task, task2];
+  }
