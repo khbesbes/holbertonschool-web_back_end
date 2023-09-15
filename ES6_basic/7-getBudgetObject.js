0e0f118681ba9238e57679262d8d33a8bbe110f2
khbesbes <5786@holbertonstudents.com>
2023-09-15 13:30:48 +0100

    7-getBudgetObject.js

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,9 +1,8 @@
 export default function getBudgetObject(income, gdp, capita) {
     const budget = {
-      income: income,
-      gdp: gdp,
-      capita: capita,
+        income,
+        gdpgdp,
+        capita,
     };
-    const {income, gdp, capita,} = budget;
-        return budget =`{ income:${income}, gdp:${gdp}, capita: ${gdp}}`;
+    return budget;
   }
