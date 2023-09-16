7a624c6697b4b05397668a54654fc775e1955238
khbesbes <5786@holbertonstudents.com>
2023-09-16 21:51:26 +0100

    ' to modification

--- ES6_basic/6-string-interpolation.js
+++ ES6_basic/6-string-interpolation.js
@@ -6,7 +6,7 @@ export default function getSanFranciscoDescription() {
       capita: '$178,479',
     };
   
-    return 'As of ${butget.year}, it was the seventh-highest income county in the United States'
-          / ', with a per capita personal income of ${income}. As of 2015, San Francisco'
-          / ' proper had a GDP of  ${budget.gdp} , and a GDP per capita of  ${capita}.';
+    return  `As of ${butget.year}, it was the seventh-highest income county in the United States
+              with a per capita personal income of ${income}. As of 2015, San Francisco
+              proper had a GDP of  ${budget.gdp} , and a GDP per capita of  ${capita}.`;
   }
