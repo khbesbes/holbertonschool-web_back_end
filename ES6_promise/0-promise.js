fb13ac7f9676aed0a0680f2f72f7be5072ef963b
khbesbes <5786@holbertonstudents.com>
2023-10-13 11:13:08 +0100

    0-promise.js+

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,11 +1,14 @@
-function getResponseFromAPI(){
-    return new  Promise ((resolve, reject) => {
-    if (true){
-        resolve();
-    } else {
-        reject();
-    }
+// Only make Promise
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      /* eslint-disable */
+        if (true) {
+          resolve();
+        } else {
+          reject();
+        }
+        /* eslint-enable */
     });
-}
-
-export default getResponseFromAPI;
+  }
+  
+  export default getResponseFromAPI;
