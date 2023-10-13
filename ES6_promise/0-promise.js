a864f2fc9f19bb89e01a409bcf19f8d5c6053df5
khbesbes <5786@holbertonstudents.com>
2023-10-13 11:38:40 +0100

    another push

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,14 +1,4 @@
-// Only make Promise
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      /* eslint-disable */
-        if (true) {
-          resolve();
-        } else {
-          reject();
-        }
-        /* eslint-enable */
-    });
-  }
-  
-  export default getResponseFromAPI;
+ return Promise.resolve();  
+}
+export default getResponseFromAPI();
