d1a4a269a443833a80bb67dbb968a0fdb6add96a
khbesbes <5786@holbertonstudents.com>
2023-10-13 11:16:22 +0100

    reform the code

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,14 +1,5 @@
-// Only make Promise
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      /* eslint-disable */
-        if (true) {
-          resolve();
-        } else {
-          reject();
-        }
-        /* eslint-enable */
-    });
+    return Promise.resolve();
   }
   
   export default getResponseFromAPI;
